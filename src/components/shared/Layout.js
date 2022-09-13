7913d32a471dfd4b14558b627d05acd40cc82d06
2sseul2 <2sseul2yoo@gmail.com>
2022-09-13 10:59:50 +0900

    Refactoring: basic header except TopBarBlock

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import Header from '../basic/header/Header';
 import Footer from '../basic/Footer/Footer';
-import styled from 'styled-components';
 import TopBarBlock from '../basic/TopBarBlock/TopBarBlock';
-
-const LayoutBlock = styled.div`
-  overflow: hidden;
-`;
 
 function Layout({ children }) {
   return (
-    <LayoutBlock>
+    <>
       <TopBarBlock />
       <Header />
       {children}
       <Footer />
-    </LayoutBlock>
+    </>
   );
 }
 
