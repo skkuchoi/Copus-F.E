a49eb43857f505b0cd1b28868ff8028990354e29
2sseul2 <2sseul2yoo@gmail.com>
2022-10-10 12:57:30 +0900

    Update a part of api function

--- src/api/search/getRightSearchResult.js
+++ src/api/search/getRightSearchResult.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
-async function getRightSearchResult(filter,keyword) {
+async function getRightSearchResult(filter, keyword) {
   const response = axios({
     url: '/article/preview',
     method: 'get',
-    data: {
+    params: {
       filter: filter,
       keyword: keyword,
     },
   });
   //console.log('response 데이터: ', response);
+  return response;
 }
 
 export default getRightSearchResult;
