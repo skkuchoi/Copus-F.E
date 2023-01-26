134301802d762f620a91c9b96f83d8232aaa7eb4
skkuchoi <cyj2017@naver.com>
2023-01-27 03:20:53 +0900

    axios baseUrl 설정

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios'
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+
+axios.defaults.baseURL = process.env.REACT_APP_API_ENDPOINT
 
 root.render(
   <React.StrictMode>
